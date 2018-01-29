d7b3fa044fc1b62b4a6a97cc516a025ba5a8d415
Mulpeter91 <robertmulpetr@gmail.com>
2018-01-29 00:15:10 +0000

    uploading project

---
+++ IdentityServer4/ConsoleClient1/Program.cs
@@ -0,0 +1,59 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleClient1
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            Thread.Sleep(6000);
+
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                Console.ReadLine();
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client1", "KHJ34K-34NKJ2-34N2J-NJ3N42");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+                Console.ReadLine();
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ IdentityServer4/Server/Config.cs
@@ -0,0 +1,72 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Test;
+
+namespace Server
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client1",
+
+                    //no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    //secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("KHJ34K-34NKJ2-34N2J-NJ3N42".Sha256())
+                    },
+
+                    //scopes that client has access to
+                    AllowedScopes = {"api1"}
+                },
+                new Client
+                {
+                    ClientId = "client2",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets =
+                    {
+                        new Secret("J4234NKE-3423-4N3KJN4K23-343".Sha256())
+                    },
+                    AllowedScopes = {"api1"}
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "Diaz",
+                    Password = "12345"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "Mulpeter",
+                    Password = "54321"
+                }
+            };
+        }
+    }
+}
